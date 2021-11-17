96bbb97c73395197d9fb12e210503ffe1448bf66
karimelbahi <karimmaster333@gmail.com>
2021-11-17 13:36:02 +0200

    Second task Sum as an extension function

--- src/main/kotlin/Task1/task1.kt
+++ src/main/kotlin/Task1/task1.kt
@@ -2,7 +2,7 @@ package main.kotlin.Task1
 
 /*
 * Checking identifier
-* 
+*
 * Implement the function that checks whether a string is a valid identifier. A valid identifier is a
 * non-empty string that starts with a letter or underscore and consists
 * of only letters, digits and underscores.*/
---
+++ src/main/kotlin/Task2/task2.kt
@@ -0,0 +1,17 @@
+package main.kotlin.Task2
+
+
+//Change the 'sum' function so that it was declared as an extension to List<Int>
+
+fun List<Int>.sum(): Int {
+    var result = 0
+    for (i in this) {
+        result += i
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val sum = listOf(1, 2, 3).sum()
+    println(sum)    // 6
+}
