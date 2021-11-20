84c7e27b57dad8ee9f04000b20f5e7df4c8f537c
karimelbahi <karimmaster333@gmail.com>
2021-11-20 11:46:49 +0200

    convert Nice String task to functional programming

--- src/main/kotlin/Task7/NiceString.kt
+++ src/main/kotlin/Task7/NiceString.kt
@@ -2,14 +2,18 @@ package main.kotlin.Task7
 
 fun String.isNice(): Boolean {
 
-    fun hasBadSubstrings(): Boolean {
+/*    fun hasBadSubstrings(): Boolean {
         if (!this.contains("bu") &&
             !this.contains("ba") &&
             !this.contains("be")) return true
         return false
-    }
+    }*/
 
-    fun hasThreeVowels(): Boolean {
+/*    fun hasBadSubstrings()= setOf("bu","ba","be").none { !this.contains(it) }*/
+
+    val hasBadSubstrings = setOf("bu", "ba", "be").all { !this.contains(it) }
+
+/*    fun hasThreeVowels(): Boolean {
 
         return count{
             it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u'
@@ -19,15 +23,29 @@ fun String.isNice(): Boolean {
 //            { c -> c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' }
 //                .count() >= 3) return true
 //        return false
-    }
+//    }*/
 
-    fun hasDoubleLetter(): Boolean {
+    /*val hasThreeVowels=count {it in setOf('a','e','i','o','u')  }>=3*/
+
+    val hasThreeVowels = count { it in "aeiou" } >= 3
+
+/*    fun hasDoubleLetter(): Boolean {
         this.zipWithNext { a, b -> if (a == b) return true }
         return false
-    }
+    }*/
 
-    if (hasBadSubstrings() && hasThreeVowels() ||
-        hasBadSubstrings() && hasDoubleLetter() ||
-        hasThreeVowels() && hasDoubleLetter()) return true
-    return false
+    /*val hasDoubleLetter = (0 until lastIndex).any { this[it] == this[it + 1] }*/
+   /* val hasDoubleLetter = this.windowed(2).any { it[0]==it[1] }*/   // sif str= abcdef window returns ab bc cd de ef ...etc
+    val hasDoubleLetter = this.zipWithNext().any{ it.first==it.second }
+
+
+/*    if (hasBadSubstrings && hasThreeVowels ||
+        hasBadSubstrings && hasDoubleLetter ||
+        hasThreeVowels && hasDoubleLetter
+    ) return true
+    return false*/
+
+    /*return listOf(hasBadSubstrings,hasDoubleLetter,hasThreeVowels).filter { it==true }.size>=2*/
+
+    return listOf(hasBadSubstrings,hasDoubleLetter,hasThreeVowels).count { it==true }>=2
 }
